0a082986a9032fd21a725336c952d64a5f4ce58b
Purvesh77 <202152332@iiitvadodara.ac.in>
2024-09-18 19:01:00 +0530

    first commit

---
+++ Fx31_Project-main/client/src/Pages/LogOut.js
@@ -0,0 +1,12 @@
+function LogOut({ history }) {
+    fetch('/auth/logout')
+        .then(res => res.json())
+        .then(res => {
+            history.push('/')
+        })
+        .catch(err => console.log(err))
+}
+
+
+
+export default LogOut;
